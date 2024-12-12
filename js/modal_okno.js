19b278874ee2bb02006a8769e06425908a73cfa2
merry7tarakan <efortunatov@gmail.com>
2024-12-12 10:48:13 +0300

    first commit

---
+++ js/modal_okno.js
@@ -0,0 +1,32 @@
+window.addEventListener("DOMContentLoaded", function () {
+  "use strict";
+
+  const cookies = document.getElementById("cookies");
+  const cookiesBtn = document.getElementById("cookies__btn");
+
+  cookiesBtn.addEventListener("click", function () {
+    cookies.style.display = "none";
+  });
+});
+
+window.addEventListener("DOMContentLoaded", function () {
+  "use strict";
+
+  const sales = document.getElementById("sales");
+  const salesBtn = document.getElementById("sales__btn");
+
+  salesBtn.addEventListener("click", function () {
+    sales.style.display = "none";
+  });
+});
+
+window.addEventListener("DOMContentLoaded", function () {
+  "use strict";
+
+  const sales = document.getElementById("regalert");
+  const salesBtn = document.getElementById("regalert__btn");
+
+  salesBtn.addEventListener("click", function () {
+    sales.style.display = "none";
+  });
+});
